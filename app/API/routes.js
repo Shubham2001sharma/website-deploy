43f9840655dfa544fa5b836f7ce2ce1c2c63c3a6
Shubham2001sharma <sharmashubu4600@gmail.com>
2024-07-09 20:03:17 +0330

    third

--- app/API/routes.js
+++ app/API/routes.js
@@ -30,7 +30,7 @@ const userSchema = new mongoose.Schema({
 const User = mongoose.model("User", userSchema);
 
 app.use(cors({
-  origin: 'http://localhost:3000'
+  origin: 'https://website-deploy-bay.vercel.app',
 }));
 app.use(express.json());
 
